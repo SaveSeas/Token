aecac5dfa8eb8e49172e92febc8827ea6de9f18b
SaveSeas <89401212+SaveSeas@users.noreply.github.com>
2021-08-27 00:24:35 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,15 @@
   <script>
-     
- 
-const tokenAddress = '0x49B9a2D12845bda30B516247d1B7f552eB4de982';
-const tokenSymbol = 'SEAS';
-const tokenDecimals = 9;
-const tokenImage = 'https://github.com/SaveSeas/Token/blob/main/Logo_Big.png';
-
-try {
-  // wasAdded is a boolean. Like any RPC method, an error may be thrown.
-  const wasAdded = await ethereum.request({
-    method: 'wallet_watchAsset',
-    params: {
-      type: 'ERC20', // Initially only supports ERC20, but eventually more!
-      options: {
-        address: tokenAddress, // The address that the token is at.
-        symbol: tokenSymbol, // A ticker symbol or shorthand, up to 5 chars.
-        decimals: tokenDecimals, // The number of decimals in the token
-        image: tokenImage, // A string url of the token logo
-      },
+     web3.currentProvider.sendAsync({
+  method: 'metamask_watchAsset',
+  params: {
+    "type":"ERC20",
+    "options":{
+      "address":"0x49B9a2D12845bda30B516247d1B7f552eB4de982",
+      "symbol":"SEAS",
+      "decimals":9,
+      "image":'https://i.ibb.co/R950hTT/Logo-Big750.png'
     },
-  });
-
-  if (wasAdded) {
-    console.log('Thanks for your interest!');
-  } else {
-    console.log('Your loss!');
-  }
-} catch (error) {
-  console.log(error);
-}
-
+  },
+  id: 20,
+}, console.log)
  </script>
